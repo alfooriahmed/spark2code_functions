69df669ef670c663ccd43a5bf9c1a23abfcfde67
Ahmed <alfooriahmed3@gmail.com>
2026-07-11 18:23:40 +0400

    Task 5 grade routing system

---
+++ Grade_Rounding_System/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Grade_Rounding_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Exam Score: ");
+            float num = float.Parse(Console.ReadLine());
+
+           // Console.WriteLine(
+               double number =  Math.Round(num);
+
+            Console.WriteLine(number);
+
+            if (number >= 60)
+            {
+                Console.WriteLine("Your score is " + number + "! You passed! your ");
+
+            }
+            else
+            {
+                Console.WriteLine("Your score is "+ number + "! You Failed!");
+            }
+
+            
+        }
+    }
+}
