8b2fb2be1e762b31374007f2f4d931f6b34b1b32
Ahmed <alfooriahmed3@gmail.com>
2026-07-11 18:08:49 +0400

    Task4 Subsription end date

---
+++ Subscription_End_Date/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Subscription_End_Date
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter The Number of free trail days: ");
+            int freeTrailDays = int.Parse(Console.ReadLine());
+
+            DateTime today = DateTime.Today;
+            DateTime EndDateOFFreeTRail = today.AddDays(freeTrailDays);
+
+            // to print the end date in format: ToString("yyyy-MM-dd")
+            string endDate = EndDateOFFreeTRail.ToString();
+            Console.WriteLine("The end date of your subscription is: " + endDate);
+            //Console.WriteLine("The end date of your subscription is: " + EndDateOFFreeTRail);
+        }
+    }
+}
