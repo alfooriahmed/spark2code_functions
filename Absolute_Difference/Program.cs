39df5f5712209a57bc76293d98e72fd2c53d58d0
Ahmed <alfooriahmed3@gmail.com>
2026-07-11 17:33:26 +0400

    Task1 Absoloute diffrence

--- Absolute_Difference/Program.cs
+++ Absolute_Difference/Program.cs
@@ -3,8 +3,18 @@
     internal class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello, my name is ahmed, my friend is hamza");
+            Console.Write("Enter The First Number: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter The Second Number: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int result = num2 - num1;
+            int absolot_value = Math.Abs(result);
+            Console.WriteLine( "your positive result: " + absolot_value);
+
         }
     }
 }
